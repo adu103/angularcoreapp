074ceb1efde0201d68d87293a19e6b3ee1da6f38
Alex <adu103@gmail.com>
2018-07-19 13:20:06 -0400

    initial

---
+++ issuesapi/Controllers/IssuesController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using issuesapi.Models;
+
+namespace issuesapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssuesController : ControllerBase
+    {
+        private readonly IssueContext _context;
+
+        public IssuesController(IssueContext context)
+        {
+            _context = context;
+
+            if (_context.Issues.Count() == 0)
+            {
+                _context.Issues.Add(new Issues { title = "title1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<Issues>> GetAll()
+        {
+            return _context.Issues.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetIssue")]
+        public ActionResult<Issues> GetById(long id)
+        {
+            var issue = _context.Issues.Find(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+            return issue;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Issues issue)
+        {
+            _context.Issues.Add(issue);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetIssue", new { id = issue.Id }, issue);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, Issues item)
+        {
+            var issue = _context.Issues.Find(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            issue.title = item.title;
+            issue.responsible = item.responsible;
+            issue.description = item.description;
+            issue.severity = item.severity;
+            issue.status = item.status;
+
+            _context.Issues.Update(issue);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var issue = _context.Issues.Find(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            _context.Issues.Remove(issue);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+
+
+}
